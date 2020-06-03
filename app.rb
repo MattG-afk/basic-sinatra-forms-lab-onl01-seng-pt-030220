5b63d962c578607374e960f6cad54212aacc1e29
Matt Gordon <bigmattsize21@gmail.com>
2020-06-03 21:28:38 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,9 +7,9 @@ class App < Sinatra::Base
   end
   
   post '/team' do
-    @name = params["name"]
-    @coach = params["coach"]
-    @pg = params ["pg"]
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params [:pg]
     @sg = params ["sg"]
     @sf = params ["sf"]
     @pf = params ["pf"]
